b34b5a55188fd26616c0570202f47756774fa1dd
Mark Haslinghuis <mark@numloq.nl>
2023-11-28 13:46:40 +0100

    Add Altitude Hold Mode

--- src/main/build/debug.c
+++ src/main/build/debug.c
@@ -116,5 +116,9 @@ const char * const debugModeNames[DEBUG_COUNT] = {
     "RC_STATS",
     "MAG_CALIB",
     "MAG_TASK_RATE",
+<<<<<<< HEAD
     "EZLANDING",
+=======
+    "ALTHOLD",
+>>>>>>> f15bfcb1a (Add Altitude Hold Mode)
 };
