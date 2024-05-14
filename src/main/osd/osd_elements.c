f0cef10e589000e661bc26f3b3086fba4de27650
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2024-05-14 03:41:46 +0200

    better alt hold mode osd name

--- src/main/osd/osd_elements.c
+++ src/main/osd/osd_elements.c
@@ -1044,7 +1044,7 @@ static void osdElementFlymode(osdElementParms_t *element)
     } else if (FLIGHT_MODE(ANGLE_MODE)) {
         strcpy(element->buff, "ANGL");
     } else if (FLIGHT_MODE(ALTHOLD_MODE)) {
-        strcpy(element->buff, "AHM ");
+        strcpy(element->buff, "ALTH");
     } else if (FLIGHT_MODE(HORIZON_MODE)) {
         strcpy(element->buff, "HOR ");
     } else if (IS_RC_MODE_ACTIVE(BOXACROTRAINER)) {
