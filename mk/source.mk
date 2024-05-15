835b5f809d6befb0ef15887ddbed5f255ba7744a
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2024-05-15 05:34:59 +0200

    fixed build

--- mk/source.mk
+++ mk/source.mk
@@ -142,6 +142,7 @@ COMMON_SRC = \
             cms/cms_menu_firmware.c \
             cms/cms_menu_gps_rescue.c \
             cms/cms_menu_gps_lap_timer.c \
+            cms/cms_menu_alt_hold.c \
             cms/cms_menu_imu.c \
             cms/cms_menu_ledstrip.c \
             cms/cms_menu_main.c \
@@ -406,6 +407,7 @@ SIZE_OPTIMISED_SRC := $(SIZE_OPTIMISED_SRC) \
             cms/cms_menu_firmware.c \
             cms/cms_menu_gps_rescue.c \
             cms/cms_menu_gps_lap_timer.c \
+            cms/cms_menu_alt_hold.c \
             cms/cms_menu_imu.c \
             cms/cms_menu_ledstrip.c \
             cms/cms_menu_main.c \
