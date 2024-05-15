6d699765e1f4cab5abbe76e63327252930249ace
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2024-05-15 22:21:20 +0200

    fixed max altitude compile error and improved OSD menu ordering

--- src/main/cms/cms_menu_alt_hold.c
+++ src/main/cms/cms_menu_alt_hold.c
@@ -103,17 +103,17 @@ const OSD_Entry cmsx_menuAltitudeHoldEntries[] =
 {
     {"--- ALTITUDE HOLD ---", OME_Label, NULL, NULL},
 
-    { "PID P",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidP, 0, 200, 1 } },
-    { "PID D",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidD, 0, 200, 1 } },
-    { "PID I",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidI, 0, 200, 1 } },
-    { "PID I MAX",    OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidIMax, 0, 50, 1 } },
-    { "PID D CUTOFF", OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidDFiltCutoffFreq, 5, 250, 1 } },
+    { "MAX ALTITUDE",      OME_UINT16, NULL, &(OSD_UINT16_t){ &altholdConfig_maxAltitude, 0, 4000, 1 } },
 
     { "MIN THRTL",         OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_minThrottle, 0, 50, 1 } },
     { "MAX THRTL",         OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_maxThrottle, 20, 100, 1 } },
     { "HOVER THRTL",       OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_hoverThrottle, 0, 100, 1 } },
 
-    { "MAX ALTITUDE",       OME_UINT16, NULL, &(OSD_UINT8_t){ &altholdConfig_hoverThrottle, 0, 4000, 1 } },
+    { "PID P",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidP, 0, 200, 1 } },
+    { "PID D",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidD, 0, 200, 1 } },
+    { "PID I",        OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidI, 0, 200, 1 } },
+    { "PID I MAX",    OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidIMax, 0, 50, 1 } },
+    { "PID D CUTOFF", OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_throttlePidDFiltCutoffFreq, 5, 250, 1 } },
 
     { "ENTER FADE T",      OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_enterFadeTimeDecisec, 0, 10, 1 } },
     { "EXIT  FADE T",      OME_UINT8, NULL, &(OSD_UINT8_t){ &altholdConfig_exitFadeTimeDecisec, 0, 30, 1 } },
