9317b3fc95986bf5f7aa79d5ddd308f67bc780aa
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-07-05 05:59:50 +0200

    update build-mob7.sh

--- build-mob7.sh
+++ build-mob7.sh
@@ -1,7 +1,28 @@
+#!/bin/bash
+
+# Enable strict error handling
 set -eux
+
+# Uncomment to clean build artifacts
 # make clean
 
+# Generate dynamic release name with git info and timestamp
 RELEASE_NAME="$(git show --pretty=format:"%h" --no-patch) at $(git rev-parse --abbrev-ref HEAD) built $(date '+%Y-%m-%d %H:%M:%S')"
-echo "$RELEASE_NAME"
+echo "Building release: $RELEASE_NAME"
 
-make CRAZYBEEF4SX1280 EXTRA_FLAGS="-D'BUILD_KEY=$(git show --pretty=format:"%h" --no-patch)' -D'RELEASE_NAME=$RELEASE_NAME' -DCLOUD_BUILD -DUSE_DSHOT -DUSE_LED_STRIP -DUSE_OSD -DUSE_OSD_SD -DUSE_TELEMETRY -DUSE_TELEMETRY_CRSF -DUSE_VTX"
+make CRAZYBEEF4SX1280 \
+  EXTRA_FLAGS=" \
+    -D'BUILD_KEY=$(git show --pretty=format:"%h" --no-patch)' \
+    -D'RELEASE_NAME=$RELEASE_NAME' \
+    -DCLOUD_BUILD \
+    -DUSE_DSHOT \
+    -DUSE_LED_STRIP \
+    -DUSE_PINIO \
+    -DUSE_OSD \
+    -DUSE_OSD_SD \
+    -DUSE_SERIALRX \
+    -DUSE_SERIALRX_CRSF \
+    -DUSE_TELEMETRY \
+    -DUSE_TELEMETRY_CRSF \
+    -DUSE_VTX \
+  "
