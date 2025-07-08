4540b3e56dda90e2cbb77fcc1177bf03fd5a8958
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-07-09 01:50:18 +0200

    added RPM FILTER osd menu

--- src/main/cms/cms_menu_imu.c
+++ src/main/cms/cms_menu_imu.c
@@ -51,6 +51,7 @@
 #include "flight/mixer.h"
 #include "flight/pid.h"
 #include "flight/pid_init.h"
+#include "flight/rpm_filter.h"
 
 #include "pg/pg.h"
 
@@ -960,6 +961,79 @@ static CMS_Menu cmsx_menuDynFilt = {
 
 #endif
 
+// RPM Filter CMS Menu Implementation
+
+#ifdef USE_RPM_FILTER
+static uint8_t  cmsx_rpm_filter_harmonics;
+static uint8_t  cmsx_rpm_filter_weights[RPM_FILTER_HARMONICS_MAX];
+static uint16_t cmsx_rpm_filter_q;
+static uint8_t  cmsx_rpm_filter_min_hz;
+static uint16_t cmsx_rpm_filter_fade_range_hz;
+static uint16_t cmsx_rpm_filter_lpf_hz;
+
+static const void *cmsx_menuRpmFilter_onEnter(displayPort_t *pDisp)
+{
+    UNUSED(pDisp);
+
+    const rpmFilterConfig_t *cfg = rpmFilterConfig();
+
+    cmsx_rpm_filter_harmonics = cfg->rpm_filter_harmonics;
+    for (int i = 0; i < RPM_FILTER_HARMONICS_MAX; i++) {
+        cmsx_rpm_filter_weights[i] = cfg->rpm_filter_weights[i];
+    }
+    cmsx_rpm_filter_q = cfg->rpm_filter_q;
+    cmsx_rpm_filter_min_hz = cfg->rpm_filter_min_hz;
+    cmsx_rpm_filter_fade_range_hz = cfg->rpm_filter_fade_range_hz;
+    cmsx_rpm_filter_lpf_hz = cfg->rpm_filter_lpf_hz;
+
+    return NULL;
+}
+
+static const void *cmsx_menuRpmFilter_onExit(displayPort_t *pDisp, const OSD_Entry *self)
+{
+    UNUSED(pDisp);
+    UNUSED(self);
+
+    rpmFilterConfigMutable()->rpm_filter_harmonics = cmsx_rpm_filter_harmonics;
+    for (int i = 0; i < RPM_FILTER_HARMONICS_MAX; i++) {
+        rpmFilterConfigMutable()->rpm_filter_weights[i] = cmsx_rpm_filter_weights[i];
+    }
+    rpmFilterConfigMutable()->rpm_filter_q = cmsx_rpm_filter_q;
+    rpmFilterConfigMutable()->rpm_filter_min_hz = cmsx_rpm_filter_min_hz;
+    rpmFilterConfigMutable()->rpm_filter_fade_range_hz = cmsx_rpm_filter_fade_range_hz;
+    rpmFilterConfigMutable()->rpm_filter_lpf_hz = cmsx_rpm_filter_lpf_hz;
+
+    return NULL;
+}
+
+static const OSD_Entry cmsx_menuRpmFilterEntries[] =
+{
+    { "-- RPM FILTER --", OME_Label, NULL, NULL },
+
+    { "HARMONICS",      OME_UINT8,  NULL, &(OSD_UINT8_t)  { &cmsx_rpm_filter_harmonics,      0,    3,    1 } },
+    { "WEIGHT 1",       OME_UINT8,  NULL, &(OSD_UINT8_t)  { &cmsx_rpm_filter_weights[0],     0,    100,  1 } },
+    { "WEIGHT 2",       OME_UINT8,  NULL, &(OSD_UINT8_t)  { &cmsx_rpm_filter_weights[1],     0,    100,  1 } },
+    { "WEIGHT 3",       OME_UINT8,  NULL, &(OSD_UINT8_t)  { &cmsx_rpm_filter_weights[2],     0,    100,  1 } },
+    { "MIN Q FACTOR",   OME_UINT16, NULL, &(OSD_UINT16_t) { &cmsx_rpm_filter_q,              10,   3000, 1 } },
+    { "MIN HZ",         OME_UINT8,  NULL, &(OSD_UINT8_t)  { &cmsx_rpm_filter_min_hz,         30,   200,  1 } },
+    { "FADE RANGE HZ",  OME_UINT16, NULL, &(OSD_UINT16_t) { &cmsx_rpm_filter_fade_range_hz,  0,    1000, 1 } },
+    { "RPM LPF HZ",     OME_UINT16, NULL, &(OSD_UINT16_t) { &cmsx_rpm_filter_lpf_hz,         100,  500,  1 } },
+
+    { "BACK", OME_Back, NULL, NULL },
+    { NULL, OME_END, NULL, NULL}
+};
+
+CMS_Menu cmsx_menuRpmFilter = {
+#ifdef CMS_MENU_DEBUG
+    .GUARD_text = "MENURPMFILT",
+    .GUARD_type = OME_MENU,
+#endif
+    .onEnter = cmsx_menuRpmFilter_onEnter,
+    .onExit = cmsx_menuRpmFilter_onExit,
+    .entries = cmsx_menuRpmFilterEntries
+};
+#endif
+
 static uint16_t cmsx_dterm_lpf1_static_hz;
 static uint16_t cmsx_dterm_lpf2_static_hz;
 static uint16_t cmsx_dterm_notch_hz;
@@ -1115,6 +1189,10 @@ static const OSD_Entry cmsx_menuImuEntries[] =
     {"FILT GLB",  OME_Submenu, cmsMenuChange,                 &cmsx_menuFilterGlobal},
 #if  (defined(USE_DYN_NOTCH_FILTER) || defined(USE_DYN_LPF)) && defined(USE_EXTENDED_CMS_MENUS)
     {"DYN FILT",  OME_Submenu, cmsMenuChange,                 &cmsx_menuDynFilt},
+#endif
+
+#ifdef USE_RPM_FILTER
+    {"RPM FILT",  OME_Submenu, cmsMenuChange,                 &cmsx_menuRpmFilter},
 #endif
 
 #ifdef USE_EXTENDED_CMS_MENUS
