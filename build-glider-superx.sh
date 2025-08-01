601c1a5436efa39bd5022626dc030e8bdb529076
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-08-01 21:53:45 +0200

    update build-glider-superx.sh

--- build-glider-superx.sh
+++ build-glider-superx.sh
@@ -1,4 +1,31 @@
+#!/bin/bash
+# Enable strict error handling
 set -eux
 
-# https://build.betaflight.com/api/builds/8c9c9e861e2d93091c94f58071b1ea00/log
-make CRAZYBEEF4SX1280 EXTRA_FLAGS="-D'BUILD_KEY=8c9c9e861e2d93091c94f58071b1ea00' -D'RELEASE_NAME=4.6.0-dev' -DCLOUD_BUILD -DUSE_DSHOT -DUSE_LED_STRIP -DUSE_OSD -DUSE_OSD_SD -DUSE_PINIO -DUSE_SERIALRX -DUSE_SERIALRX_CRSF -DUSE_SERVOS -DUSE_TELEMETRY -DUSE_TELEMETRY_CRSF -DUSE_VTX -DUSE_WING"
+# Uncomment to clean build artifacts
+# make clean
+
+# Generate dynamic release name with git info and timestamp
+RELEASE_NAME="$(git show --pretty=format:"%h" --no-patch) at $(git rev-parse --abbrev-ref HEAD) built $(date '+%Y-%m-%d %H:%M:%S')"
+echo "Building release: $RELEASE_NAME"
+
+make CRAZYBEEF4SX1280 \
+  EXTRA_FLAGS=" \
+    -D'BUILD_KEY=$(git show --pretty=format:"%h" --no-patch)' \
+    -D'RELEASE_NAME=$RELEASE_NAME' \
+    -DCLOUD_BUILD \
+    -DUSE_DSHOT \
+    -DUSE_LED_STRIP \
+    -DUSE_OSD \
+    -DUSE_OSD_SD \
+    -DUSE_PINIO \
+    -DUSE_SERIALRX \
+    -DUSE_SERIALRX_CRSF \
+    -DUSE_SERVOS \
+    -DUSE_TELEMETRY \
+    -DUSE_TELEMETRY_CRSF \
+    -DUSE_VTX \
+    -DUSE_WING \
+  "
+
+cp ./obj/betaflight_4.6.0_STM32F411_CRAZYBEEF4SX1280.hex ~/Downloads/.
