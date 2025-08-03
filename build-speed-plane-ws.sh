555acfbf1f06990c263f46cea215066532cff695
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-08-03 05:09:30 +0200

    update build script for speed plane

--- build-speed-plane-ws.sh
+++ build-speed-plane-ws.sh
@@ -1,4 +1,35 @@
+#!/bin/bash
+# Enable strict error handling
 set -eux
 
-# https://build.betaflight.com/api/builds/1971e617a0ac8fd24a57676c8ddebc81/log
-make SPEEDYBEEF405MINI EXTRA_FLAGS="-D'BUILD_KEY=1971e617a0ac8fd24a57676c8ddebc81' -D'RELEASE_NAME=4.6.0-dev' -DCLOUD_BUILD -DUSE_ALTITUDE_HOLD -DUSE_DSHOT -DUSE_GPS -DUSE_GPS_PLUS_CODES -DUSE_LED_STRIP -DUSE_OSD_HD -DUSE_PINIO -DUSE_POSITION_HOLD -DUSE_SERIALRX -DUSE_SERIALRX_CRSF -DUSE_SERVOS -DUSE_SOFTSERIAL -DUSE_TELEMETRY -DUSE_TELEMETRY_CRSF -DUSE_VTX -DUSE_WING"
+# Uncomment to clean build artifacts
+# make clean
+
+# Generate dynamic release name with git info and timestamp
+RELEASE_NAME="$(git show --pretty=format:"%h" --no-patch) at $(git rev-parse --abbrev-ref HEAD) built $(date '+%Y-%m-%d %H:%M:%S')"
+echo "Building release: $RELEASE_NAME"
+
+make SPEEDYBEEF405MINI \
+  EXTRA_FLAGS=" \
+    -D'BUILD_KEY=$(git show --pretty=format:"%h" --no-patch)' \
+    -D'RELEASE_NAME=$RELEASE_NAME' \
+    -DCLOUD_BUILD \
+    -DUSE_ALTITUDE_HOLD \
+    -DUSE_DSHOT \
+    -DUSE_GPS \
+    -DUSE_GPS_PLUS_CODES \
+    -DUSE_LED_STRIP \
+    -DUSE_OSD_HD \
+    -DUSE_PINIO \
+    -DUSE_POSITION_HOLD \
+    -DUSE_SERIALRX \
+    -DUSE_SERIALRX_CRSF \
+    -DUSE_SERVOS \
+    -DUSE_SOFTSERIAL \
+    -DUSE_TELEMETRY \
+    -DUSE_TELEMETRY_CRSF \
+    -DUSE_VTX \
+    -DUSE_WING \
+  "
+
+cp ./obj/betaflight_4.6.0_STM32F405_SPEEDYBEEF405MINI.hex ~/Downloads/.
