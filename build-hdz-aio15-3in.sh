82a5591fdfded485b7023ac0583bb80f13d52e4b
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-08-24 01:42:54 +0200

    pretty build-common.sh

--- build-hdz-aio15-3in.sh
+++ build-hdz-aio15-3in.sh
@@ -1,50 +1,27 @@
 #!/bin/bash
 
-# Enable strict error handling
-set -eux
+set -eu
+source build-common.sh
 
-# Target configuration
 TARGET="HDZERO_AIO15"
 
-# Uncomment to clean build artifacts
-# make clean
+DEFINES=(
+  "CLOUD_BUILD"
+  "USE_DSHOT"
+  "USE_LED_STRIP"
+  "USE_PINIO"
+  "USE_OSD"
+  "USE_OSD_HD"
+  "USE_SERIALRX"
+  "USE_SERIALRX_CRSF"
+  "USE_TELEMETRY"
+  "USE_TELEMETRY_CRSF"
+  "USE_VTX"
+  "USE_TELEMETRY_SMARTPORT"
+  "USE_BLACKBOX"
+  "DEFAULT_BLACKBOX_DEVICE=BLACKBOX_DEVICE_SERIAL"
+)
 
-# Generate dynamic build info
-BUILD_KEY="$(git show --pretty=format:"%h" --no-patch)"
-RELEASE_NAME="$BUILD_KEY at $(git rev-parse --abbrev-ref HEAD) built $(date '+%Y-%m-%d %H:%M:%S')"
+build_target "$TARGET" "${DEFINES[@]}"
 
-echo "Building target: $TARGET"
-echo "Release: $RELEASE_NAME"
-
-# Build the target
-make "$TARGET" \
-  EXTRA_FLAGS=" \
-    -D'BUILD_KEY=$BUILD_KEY' \
-    -D'RELEASE_NAME=$RELEASE_NAME' \
-    -DCLOUD_BUILD \
-    -DUSE_DSHOT \
-    -DUSE_LED_STRIP \
-    -DUSE_PINIO \
-    -DUSE_OSD \
-    -DUSE_OSD_HD \
-    -DUSE_SERIALRX \
-    -DUSE_SERIALRX_CRSF \
-    -DUSE_TELEMETRY \
-    -DUSE_TELEMETRY_CRSF \
-    -DUSE_VTX \
-    -DUSE_TELEMETRY_SMARTPORT \
-    -DUSE_BLACKBOX \
-    -D'DEFAULT_BLACKBOX_DEVICE=BLACKBOX_DEVICE_SERIAL' \
-  "
-
-# Find and copy the generated hex file
-HEX_FILE=$(find ./obj -name "betaflight_*_${TARGET}.hex" -type f | head -1)
-
-if [ -n "$HEX_FILE" ] && [ -f "$HEX_FILE" ]; then
-    echo "Found hex file: $HEX_FILE"
-    cp "$HEX_FILE" ~/Downloads/
-    echo "Copied to: ~/Downloads/$(basename "$HEX_FILE")"
-else
-    echo "Error: Could not find hex file matching pattern betaflight_*_${TARGET}.hex in ./obj"
-    exit 1
-fi
+copy_hex_to_downloads "$TARGET"
