f9e7ef86a06f3010065a987d08f32e8fc2cc2b61
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-09-20 06:30:14 +0200

    added nice comments about actions setup in edgetx

--- src/main/fc/rc_actions.h
+++ src/main/fc/rc_actions.h
@@ -27,23 +27,38 @@
 #include "pg/pg.h"
 
 typedef enum {
-    RC_ACTION_NONE = 0,
-    RC_ACTION_ADJUSTMENT_NEXT,
-    RC_ACTION_ADJUSTMENT_PREV,
-    RC_ACTION_ADJUSTMENT_INCREASE,
-    RC_ACTION_ADJUSTMENT_DECREASE,
-    RC_ACTION_OSD_NEXT,
-    RC_ACTION_OSD_PREV,
-    RC_ACTION_OSD_TOGGLE,
-    RC_ACTION_VTX_TOGGLE,
-    RC_ACTION_BEEPER_TOGGLE,
-    RC_ACTION_LED_STRIP_TOGGLE,
-    RC_ACTION_PID_PROFILE_NEXT,
-    RC_ACTION_PID_PROFILE_PREV,
-    RC_ACTION_PID_PROFILE_SAVE,
-    RC_ACTION_PID_PROFILE_RESET,
-    RC_ACTION_BB_TOGGLE,
-    RC_ACTION_COUNT,
+    // RC Action values for EdgeTX Special Function configuration
+    // These values correspond to EdgeTX "override" settings for AUX channels
+    // 
+    // Setup instructions:
+    // 1. In EdgeTX, create a Special Function with "Override Channel" action
+    // 2. Set the channel to your desired AUX channel (commonly AUX8)
+    // 3. Ensure the AUX channel is at center position with no mixes applied
+    // 4. Use the values shown in comments below as the override value
+    // 5. Bind this Special Function to a switch/button to trigger the action
+    // 
+    // Note: Current values are negative, but future additions may use positive values
+    //
+    // Warning: This will only work with ELRS "Wide" Switching mode and Std Telemetry Ratio 
+    // (to guarantee resolution of 128 for AUX8).
+    
+    RC_ACTION_NONE = 0,                    // No action (neutral position should be 0 which shows as 1503 in Betaflight Configurator "Receiver" tab)
+    RC_ACTION_ADJUSTMENT_NEXT,             // -97: Cycle to next adjustment setting
+    RC_ACTION_ADJUSTMENT_PREV,             // -96: Cycle to previous adjustment setting  
+    RC_ACTION_ADJUSTMENT_INCREASE,         // -94: Increase current adjustment value
+    RC_ACTION_ADJUSTMENT_DECREASE,         // -92: Decrease current adjustment value
+    RC_ACTION_OSD_NEXT,                    // -91: Switch to next OSD screen/page
+    RC_ACTION_OSD_PREV,                    // -89: Switch to previous OSD screen/page
+    RC_ACTION_OSD_TOGGLE,                  // -88: Toggle OSD on/off
+    RC_ACTION_VTX_TOGGLE,                  // -86: Toggle VTX power/channel settings
+    RC_ACTION_BEEPER_TOGGLE,               // -85: Toggle beeper on/off
+    RC_ACTION_LED_STRIP_TOGGLE,            // -83: Toggle LED strip on/off
+    RC_ACTION_PID_PROFILE_NEXT,            // -82: Switch to next PID profile
+    RC_ACTION_PID_PROFILE_PREV,            // -80: Switch to previous PID profile
+    RC_ACTION_PID_PROFILE_SAVE,            // -79: Save current PID profile to EEPROM
+    RC_ACTION_PID_PROFILE_RESET,           // -77: Reset current PID profile to defaults
+    RC_ACTION_BB_TOGGLE,                   // -76: Toggle blackbox logging on/off
+    RC_ACTION_COUNT,                       // Total number of actions (for validation)
 } rcAction_e;
 
 // this is limited by ELRS channel resolution
