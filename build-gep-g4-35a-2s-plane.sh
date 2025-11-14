4c4cb3a0962d2031f6e6b50a9fe14e4f5d3ff7fa
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2025-11-14 04:08:48 +0100

    build-gep-g4-35a-2s-plane.sh

---
+++ build-gep-g4-35a-2s-plane.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+source build-common.sh
+
+TARGET="TAKERG4AIO"
+
+DEFINES=(
+  "CLOUD_BUILD"
+  "USE_DSHOT"
+  "USE_GPS"
+  "USE_GPS_PLUS_CODES"
+  "USE_LED_STRIP"
+  "USE_PINIO"
+  "USE_ALTITUDE_HOLD"
+  "USE_POSITION_HOLD"
+  "USE_OSD"
+  "USE_OSD_HD"
+  "USE_VTX"
+  "USE_WING"
+)
+
+build_target "$TARGET" "${DEFINES[@]}"
+
+copy_hex_to_downloads "$TARGET"
